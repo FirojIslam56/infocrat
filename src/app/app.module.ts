8e2d5353d2c3d6b2ddfb676c1dffeea1e150947b
FirojIslam56 <firoj.cse3238@gmail.com>
2023-09-16 20:43:50 +0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+import { OurservicesComponent } from './components/ourservices/ourservices.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BannerComponent,
+    AboutComponent,
+    ProductsComponent,
+    OurservicesComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
